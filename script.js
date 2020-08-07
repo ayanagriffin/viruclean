62d1c09c76162b7753993414debbf952d96d4abf
Glitch (hello-webpage) <none>
2020-08-07 18:23:03 +0000

    🔋💁 Checkpoint ./script.js:18280517/952 ./.glitch-assets:18280517/532

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /*global createCanvas, imageMode, random, image, ellipse, CORNERS, colorMode, loadImage, textSize, getAudioContext, loadFont, textFont, textAlign, text, noStroke, HSB, background, collideRectCircle, mouseX, mouseY, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
 
 
-let canvas, livingRoomImg, imgX, imgY, finalImg, vx, vy, viruses;
+let canvas, livingRoomImg, imgX, imgY, finalImg, virusImg, viruses;
 
-
+function preload(){
+  virusImg = loadImage("https://cdn.glitch.com/b409a92a-1f80-49e0-a812-620661773dbd%2F6bb45751-0572-4522-8e4d-c9c572b8fe52_smilie-4901128_960_720.png?v=1596824410604");
+}
 function setup() {
   livingRoomImg = loadImage("https://cdn.glitch.com/b409a92a-1f80-49e0-a812-620661773dbd%2Fliving-room-interior-cartoon-vector-20941629.jpg?v=1596816373329")
   canvas = createCanvas(400, 400);
@@ -13,13 +15,8 @@ function setup() {
   imgY = height / 2;
   image(livingRoomImg, imgX, imgY)
   livingRoomImg.resize(windowWidth, 0)
-  
-//   vx = random(-windowWidth * .3, windowWidth * .85);
-
-//   vy = random(-livingRoomImg.height *.15, livingRoomImg.height);
- 
   viruses = [];
-  for(let i = 0; i < 3; i++){
+  for(let i = 0; i < 5; i++){
     viruses.push(new Virus());
   }
   
@@ -29,10 +26,7 @@ function draw() {
   background(95);
   imageMode(CENTER)
   image(livingRoomImg, imgX, imgY)
-  //livingRoomImg.resize(windowWidth, 0)
  
- // console.log(livingRoomImg.width, livingRoomImg.height)
- // console.log(imgX, imgY);
   checkMousePosition();
   for(let i = 0; i < viruses.length; i++){
     viruses[i].show();
@@ -48,30 +42,26 @@ function checkMousePosition(){
   let endY = imgY + livingRoomImg.height / 2
   let xMove = 0;
   let yMove = 0;
-  //console.log(vx, endX);
-  // console.log("mouse" + mouseX, mouseY)
-  // console.log(vx, vy)
+
   if(mouseX > width  && endX > width) {
-    
-    //imgX -= 1;
-    xMove = -1;
-    //vx-=1;
-    
+  
+    xMove = -5;
+ 
   }else if(mouseX < 0 && endX < windowWidth){
-   // imgX += 1;
-    xMove = 1;
-    // vx+=1
+  
+    xMove = 5;
+   
 
   }
   
   if(mouseY > height && endY > height){
-   // imgY -= 1;
-    yMove = -1;
-   // vy -=1
+  
+    yMove = -5;
+ 
   }else if(mouseY < 0   && endY < livingRoomImg.height){
-   // imgY += 1;
-    yMove = 1;
-   // vy+=1
+ 
+    yMove = 5;
+  
   }
   
   imgX += xMove;
@@ -84,13 +74,15 @@ function checkMousePosition(){
 
 class Virus{
   constructor(){
-    this.x = random(-windowWidth * .3, windowWidth * .85);
-    this.y = random(-livingRoomImg.height *.15, livingRoomImg.height);
+    this.x = random(-windowWidth * .15, windowWidth * .85);
+    this.y = random(-livingRoomImg.height *.3, livingRoomImg.height);
+    
     this.size = random(10, 30);
-    console.log(this.x, this.y);
+  
   }
   
   show(){
+    image(virusImg, th)
     fill("red")
     ellipse(this.x, this.y, this.size);
   }
