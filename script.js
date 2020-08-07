8ff4f4bfe0bf9ae5191d7eddfd71036b9e412054
Glitch (hello-webpage) <none>
2020-08-07 02:28:54 +0000

    🏯🏙 Checkpoint ./script.js:18280517/747

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/*global createCanvas, colorMode, HSB, background, collideRectCircle, mouseX, mouseY, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
+/*global createCanvas, colorMode, noStroke, HSB, background, collideRectCircle, mouseX, mouseY, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
 
-let alarmSound, bgColor, centerButtonIsVisible = true, centerButtonX , 
-    centerButtonY, centerButtonW , centerButtonH, centerButtonClicked, centerButtonFill ;
+let alarmSound, bgColor = [0], centerButtonIsVisible = true, centerButtonX , 
+    centerButtonY, buttonW , buttonH, centerButtonClicked, centerButtonFill ;
 
 function preload(){
   soundFormats("mp3");
@@ -14,11 +14,9 @@ function setup() {
   colorMode(HSB);
   centerButtonX = width / 2
   centerButtonY = height / 2
-  centerButtonW = width / 6
-  centerButtonH = height / 10
-  centerButtonFill = (90, 90, 90);
-  bgColor = (255, 0, 255);
-  centerButtonFill = (100, 100, 100);
+  buttonW = width / 5
+  buttonH = height / 10
+  centerButtonFill = [5, 50, 100];
   //alarmSound.play();
 }
 
@@ -28,23 +26,35 @@ function draw() {
   
 }
 
-function mouseClicked(){
+function mousePressed(){
   
-  centerButtonClicked = collideRectCircle(centerButtonX - centerButtonW/2, centerButtonY - centerButtonH/2, centerButtonW, centerButtonH, mouseX, mouseY, 10);
+  centerButtonClicked = collideRectCircle(centerButtonX - buttonW/2, centerButtonY - buttonH/2, buttonW, buttonH, mouseX, mouseY, 10);
   if(centerButtonIsVisible && centerButtonClicked){
+    
     console.log("clicked")
   }
+  
   alarmSound.stop();
 }
 
+function mouseReleased(){
+  if(centerButtonIsVisible){
+    centerButtonClicked = false;
+  }
+}
 
 function drawButtons(){
   rectMode(CENTER);
-  
+  noStroke();
   
   if(centerButtonIsVisible){
+    if(!centerButtonClicked){
+      fill(5, 60, 100);
+      rect(centerButtonX, centerButtonY + 5, buttonW, buttonH, 10);
+      
+    }
     fill(centerButtonFill);
-    rect(centerButtonX, centerButtonY, centerButtonW, centerButtonH, 10);
+    rect(centerButtonX, centerButtonY, buttonW, buttonH, 10);
     
     
   }
