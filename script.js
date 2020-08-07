d187c04271a225d0c1d473b6a5e26e440feff7b9
Glitch (hello-webpage) <none>
2020-08-07 18:58:55 +0000

    🚕💐 Checkpoint ./script.js:18280517/13457

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 /*global createCanvas, imageMode, random, image, collidePointCircle, ellipse, CORNERS, colorMode, loadImage, textSize, getAudioContext, loadFont, textFont, textAlign, text, noStroke, HSB, background, collideRectCircle, mouseX, mouseY, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
 
-let canvas, livingRoomImg, imgX, imgY, finalImg, virusImg, viruses;
+let canvas,
+  livingRoomImg,
+  imgX,
+  imgY,
+  finalImg,
+  virusImg,
+  viruses,
+  userIsFighting = false,
+  currentVirus;
 
 function preload() {
   livingRoomImg = loadImage(
@@ -20,7 +28,7 @@ function setup() {
   image(livingRoomImg, imgX, imgY);
   livingRoomImg.resize(windowWidth, 0);
   viruses = [];
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 3; i++) {
     viruses.push(new Virus());
   }
 }
@@ -36,36 +44,44 @@ function draw() {
   }
 }
 
-function mouseClicked(){
-  for (let i = 0; i < viruses.length; i++) {
-    viruses[i].checkClicked();
+function mouseClicked() {
+  if (!userIsFighting) {
+    for (let i = 0; i < viruses.length; i++) {
+      if (viruses[i].checkClicked()) {
+        currentVirus = viruses[i];
+        userIsFighting = true;
+        console.log(currentVirus);
+      }
+    }
   }
-  
 }
 
-
 function checkMousePosition() {
-  let endX = imgX + windowWidth / 2;
-  let endY = imgY + livingRoomImg.height / 2;
-  let xMove = 0;
-  let yMove = 0;
+  if (!userIsFighting) {
+    let endX = imgX + windowWidth / 2;
+    let endY = imgY + livingRoomImg.height / 2;
+    let xMove = 0;
+    let yMove = 0;
 
-  if (mouseX > width && endX > width) {
-    xMove = -5;
-  } else if (mouseX < 0 && endX < windowWidth) {
-    xMove = 5;
-  }
+    if (mouseX > width && endX > width) {
+      xMove = -5;
+    } else if (mouseX < 0 && endX < windowWidth) {
+      xMove = 5;
+    }
 
-  if (mouseY > height && endY > height) {
-    yMove = -5;
-  } else if (mouseY < 0 && endY < livingRoomImg.height) {
-    yMove = 5;
-  }
+    if (mouseY > height && endY > height) {
+      yMove = -5;
+    } else if (mouseY < 0 && endY < livingRoomImg.height) {
+      yMove = 5;
+    }
 
-  imgX += xMove;
-  imgY += yMove;
-  for (let i = 0; i < viruses.length; i++) {
-    viruses[i].move(xMove, yMove);
+    imgX += xMove;
+    imgY += yMove;
+    for (let i = 0; i < viruses.length; i++) {
+      viruses[i].move(xMove, yMove);
+    }
+  }else{
+    
   }
 }
 
@@ -73,28 +89,45 @@ class Virus {
   constructor() {
     this.size = 50;
     this.x = random(-windowWidth * 0.3, windowWidth * 0.65);
-    this.y = random(-livingRoomImg.height * .05 + this.size, livingRoomImg.height * .7 - this.size);
+    this.y = random(
+      -livingRoomImg.height * 0.05 + this.size,
+      livingRoomImg.height * 0.7 - this.size
+    );
     this.isAttacked = false;
-   
-    
-  
+    this.maxSize = 500;
   }
 
   show() {
-    imageMode(CENTER)
+    imageMode(CENTER);
     image(virusImg, this.x, this.y, this.size, this.size);
+
+    if (this.isAttacked) {
+      this.grow();
+    }
   }
 
+  grow() {
+    if (this.size < this.maxSize) {
+      this.size += 0.25;
+    }
+  }
   move(xResult, yResult) {
     this.x += xResult;
     this.y += yResult;
-    
   }
-  
-  checkClicked(){
-    let virusClicked = collidePointCircle(mouseX, mouseY, this.x, this.y, this.size)
-    if(virusClicked){
-      console.log("hit");
+
+  checkClicked() {
+    let virusClicked = collidePointCircle(
+      mouseX,
+      mouseY,
+      this.x,
+      this.y,
+      this.size
+    );
+    if (virusClicked) {
+      this.isAttacked = true;
     }
+
+    return virusClicked;
   }
 }
