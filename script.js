01a214ad259c0641462a9da7431cbb65b64854cd
Glitch (hello-webpage) <none>
2020-08-07 17:25:35 +0000

    🙆🏓 Checkpoint ./script.js:18280517/329 ./style.css:18280517/1390 ./index.html:18280517/34 ./.glitch-assets:18280517/487

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /*global createCanvas, imageMode, image, CORNERS, colorMode, loadImage, textSize, getAudioContext, loadFont, textFont, textAlign, text, noStroke, HSB, background, collideRectCircle, mouseX, mouseY, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
 
 
-let livingRoomImg, imgX, imgY, finalImg;
+let canvas, livingRoomImg, imgX, imgY, finalImg;
 
 
 function setup() {
-  livingRoomImg = loadImage("https://cdn.glitch.com/b409a92a-1f80-49e0-a812-620661773dbd%2Fliving-room-interior-cartoon-vector-20941629.jpg?v=1596816373329")
-  createCanvas(400, 400);
+  livingRoomImg = loadImage("https://cdn.glitch.com/b409a92a-1f80-49e0-a812-620661773dbd%2FScreen%20Shot%202020-08-07%20at%2010.24.42%20AM.png?v=1596821093223")
+  canvas = createCanvas(400, 400);
+  canvas.parent("canvas-div");
   colorMode(HSB);
-  console.log(400, 400)
   imgX = width / 2;
   imgY = height / 2;
   
