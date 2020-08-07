b1966b28fba21b7ba24646f58495348e11e0c519
Glitch (hello-webpage) <none>
2020-08-07 01:25:55 +0000

    💐👷 Checkpoint ./script.js:18280517/810 ./.glitch-assets:18280517/364

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-/*global createCanvas, colorMode, HSB, background, windowWidth, windowHeight*/
+/*global createCanvas, colorMode, HSB, background, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
 
-let variables;
+let alarmSound, backgroundColor = "black", centerButtonIsVisible = true;
+
+function preload(){
+  soundFormats("mp3");
+  alarmSound = loadSound("https://cdn.glitch.com/aaab3da2-4498-416a-b626-0e83a89f16f3%2FAlarm-ringtone.mp3?v=1596763108789");
+}
 
 function setup() {
-  createCanvas(windowWidth, 400);
+  createCanvas(windowWidth * 2 / 3, windowHeight * 2 / 3);
   colorMode(HSB, 360, 100, 100);
+  //alarmSound.play();
 }
 
 function draw() {
-  background(95);
+  background(backgroundColor);
+  drawButtons();
   
 }
 
+function mouseClicked(){
+  alarmSound.stop();
+}
+
+
+function drawButtons(){
+  rectMode(CENTER);
+  fill("red");
+  
+  if(centerButtonIsVisible){
+    rect(width / 2, height / 2, width / 6, height / 10, 10);
+    
+  }
+  
+}
 
 
