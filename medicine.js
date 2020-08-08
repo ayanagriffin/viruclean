3c7db934d20e90cbd75ac285ab7c95123a66a779
Glitch (melonjam-pt2) <none>
2020-08-08 15:28:37 +0000

    🙋🐪 Checkpoint ./medicine.js:18280517/885

---
+++ medicine.js
@@ -0,0 +1,18 @@
+/*global createCanvas, font, handleTime, handleHealth, livingRoomImg, timerCushion, healthCushion, checkMousePosition, title, drawButtons, level, numViruses, timer, health, infectedViruses, viruses, gameIsOver, userIsInfected, imgX, imgY, gameOverText, imageMode, playScreenSetup, drawStartScreen, drawEndScreen, Button, drawPlayScreen, collidePointRect, Virus, stroke, strokeWeight, LEFT, RIGHT, round, textAlign, rectMode, CORNER, random, key, image, collidePointCircle, ellipse, CORNERS, colorMode, loadImage, textSize, getAudioContext, loadFont, textFont, textAlign, text, noStroke, HSB, background, collideRectCircle, mouseX, mouseY, fill, windowWidth, windowHeight, width, height, soundFormats, loadSound, rect, rectMode, CENTER*/
+
+class Medicine{
+  constructor(){
+    this.x = 1;
+    this.y = 1;
+    this.size = 1;
+    
+  }
+  
+  show(){
+    
+  }
+  
+  animate(){
+    
+  }
+}
